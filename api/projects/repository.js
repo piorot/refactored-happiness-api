02d8e1af7c52bd7efcfb619e8b4ddf3408789fb6
piorot <rotynski@gmail.com>
2017-11-15 21:26:59 +0100

    CHG Repository change

--- api/projects/repository.js
+++ api/projects/repository.js
@@ -1,10 +1,29 @@
 'use strict'
 var Projects = require('./model')
 
-exports.listAll = function (req, res) {
+function listAll (req, res) {
   return Projects.find()
 }
 
-exports.getProjectById = function (id) {
+function getProjectById (id) {
   return Projects.find({ id: id }).then((projectArr) => projectArr[0])
+}
+
+function create (category) {
+  var entry = new Projects({
+    customerDetails: { 'type': Object },
+    name: { 'type': String },
+    description: { 'type': String },
+    teamsAssigned: { 'type': Array },
+    techStack: { 'type': Array },
+    icon: { 'type': String },
+    splashScreen: { 'type': String }
+  })
+  return entry.save()
+}
+
+module.exports = {
+  listAll: listAll,
+  getProjectById: getProjectById,
+  create: create
 }
